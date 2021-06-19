6e6133e06e34bc38e2889c6332817a2ab259b87b
Franck Nijhof <git@frenck.dev>
2021-06-19 16:51:04 +0200

    Add support for WLED presets

--- examples/control.py
+++ examples/control.py
@@ -3,7 +3,7 @@
 
 import asyncio
 
-from wled import WLED
+from wled import WLED, Preset
 
 
 async def main():
@@ -11,6 +11,9 @@ async def main():
     async with WLED("10.10.11.135") as led:
         device = await led.update()
         print(device.info.version)
+
+        if isinstance(device.state.preset, Preset):
+            print(f"Preset active! Name: {device.state.preset.name}")
 
         # Turn strip on, full brightness
         await led.master(on=True, brightness=255)
