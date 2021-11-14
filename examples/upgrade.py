3ac7e804632293a237f2b6c0d431755c6eea47e2
Franck Nijhof <git@frenck.dev>
2021-11-14 10:17:25 +0100

    Extend upgrade example

--- examples/upgrade.py
+++ examples/upgrade.py
@@ -10,9 +10,15 @@ async def main():
     """Show example on upgrade your WLED device."""
     async with WLED("10.10.11.54") as led:
         device = await led.update()
-        print(device.info)
+        print(f"Latest stable version: {device.info.version_latest_stable}")
+        print(f"Latest beta version: {device.info.version_latest_beta}")
+        print(f"Current version: {device.info.version}")
 
+        print("Upgrading WLED....")
         await led.upgrade(version="0.13.0-b4")
+
+        device = await led.update()
+        print(f"Current version: {device.info.version}")
 
 
 if __name__ == "__main__":
