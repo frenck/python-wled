25783b69f9ef0a2408cca6c243458e1a0076cdf9
James Myatt <james@jamesmyatt.co.uk>
2024-07-17 23:12:41 +0100

    Make architecture lower case before its stored

--- src/wled/models.py
+++ src/wled/models.py
@@ -489,7 +489,7 @@ class Info(BaseModel):  # pylint: disable=too-many-instance-attributes
             obj.websocket = None
 
         # We can tweak the architecture name based on the filesystem size.
-        if obj.filesystem is not None and obj.architecture.lower() == "esp8266":
+        if obj.filesystem is not None and obj.architecture == "esp8266":
             if obj.filesystem.total <= 256:
                 obj.architecture = "esp01"
             elif obj.filesystem.total <= 512:
@@ -807,6 +807,8 @@ class Device(BaseModel):
             self.playlists.pop(0, None)
 
         if _info := data.get("info"):
+            if "architecture" in _info:
+                _info["architecture"] = _info["architecture"].lower()
             self.info = Info.from_dict(_info)
 
         if _state := data.get("state"):
